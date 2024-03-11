c2779f88aa764c8ead5cd3ebe72b8ff3077397ad
Maiden23 <angelmariastanly@gmail.com>
2024-03-11 20:29:50 +0530

    all folders r converted to images

--- Hand_sign.py
+++ Hand_sign.py
@@ -1,8 +1,18 @@
 import glob
 import nltk
 import os
+import numpy as np
 
-for files in os.listdir("C:/Users/angel/OneDrive/Documents/3rd year/sem2/NLP/Hand-Sign-Gestures\Dataset of Common phrases in Indian Sign Language/MP_Data"):
+for files in os.listdir("C:/Users/angel/OneDrive/Documents/3rd year/sem2/NLP/Hand-Sign-Gestures/Dataset of Common phrases in Indian Sign Language/MP_Data"):
     print(files)
 
-filenames = glob.glob("*.npy")
+import matplotlib
+
+from pathlib import Path
+
+#Reading All the Folders 
+all_folders = Path("C:/Users/angel/OneDrive/Documents/3rd year/sem2/NLP/Hand-Sign-Gestures/Dataset of Common phrases in Indian Sign Language/MP_Data")
+all_files = list(all_folders.rglob(".*npy"))
+for file in all_files:
+    all_folder = np.load(file)
+    print(all_folder)
